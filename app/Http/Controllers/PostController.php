d60a87c13b887c9ac173e34c68fb247c5372e71e
The Mrb <60550793+themrb8@users.noreply.github.com>
2023-03-15 22:55:59 +0600

    Section 2 | Backend, Sanctum, Middleware, Authorization | Complete Process

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        return Post::latest()->get();
+        return Post::with('user:id,name')->latest()->get();
     }
 
     /**
@@ -36,7 +36,7 @@ class PostController extends Controller
         ]);
 
         return Post::create([
-            'user_id' => 1,
+            'user_id' => auth()->id(),
             'title' => $request->title,
             'body' => $request->body,
         ]);
@@ -47,7 +47,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        return $post;
+        return $post->load('user:id,name');
     }
 
     /**
@@ -55,15 +55,26 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        $this->authorize('update', $post);
+        return $post->load('user:id,name');
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(Request $request, Post $post)
     {
-        //
+        $this->authorize('update', $post);
+
+        $request->validate([
+            'title' => 'required|min:3',
+            'body' => 'required|min:3'
+        ]);
+
+        return $post->update([
+            'title' => $request->title,
+            'body' => $request->body,
+        ]);
     }
 
     /**
@@ -71,6 +82,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $this->authorize('delete', $post);
+
+        $post->delete();
     }
 }
